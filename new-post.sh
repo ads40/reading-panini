451bedd0dfd0fb40482ee9272f66b16e0b8d1bea
Alex Stevens <ads40@cantab.net>
2025-04-23 20:23:37 +1000

    Update shell script for new posts.

--- new-post.sh
+++ new-post.sh
@@ -3,18 +3,21 @@
 # Prompt for the slug and title
 read -p "Enter the slug (e.g., my-new-post): " slug
 read -p "Enter the title: " title
+read -p "Enter the category (posts, texts, biblio, glossary): " category
 
 # Get today's date
-date=$(date +"%Y-%m-%d")
+date=$(date +"%Y-%m-%d")s
 
 # Define the file path
-file_path="content/${slug}.md"
+file_path="content/${category}/${slug}.md"
 
 # Create the new markdown file with front matter
 cat <<EOL > $file_path
 +++
 title = "${title}"
 date = ${date}
+[taxonomies]
+tags = ["${category}"]
 +++
 
 EOL
